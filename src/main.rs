44939d3b84d282bda4984dd018cc2fa1fee82ddf
KPMGE <jesuskevin254@gmail.com>
2022-12-17 23:39:35 -0300

    :sparkles: add example with dynamic params

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,12 @@ fn hello() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/hello/<name>/<age>")]
+fn greet(name: &str, age: u8) -> String {
+    format!("Hello, {} your age is {}, isn't it?", name, age)
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello])
+    rocket::build().mount("/", routes![hello, greet])
 }
