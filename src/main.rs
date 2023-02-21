b019437b5300ec22827dc592b60ced078ae78ee8
KPMGE <jesuskevin254@gmail.com>
2023-02-20 23:46:59 -0300

    :sparkles: add example with json

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use rocket::serde::{Serialize, json::Json};
+
 #[macro_use] extern crate rocket;
 
 #[get("/")]
@@ -10,7 +12,22 @@ fn greet(name: &str, age: u8) -> String {
     format!("Hello, {} your age is {}, isn't it?", name, age)
 }
 
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct Person {
+    name: String,
+    age: i32
+}
+
+#[get("/person")]
+fn get_person() -> Json<Person> {
+    Json(Person {  
+        name: "kevin".to_string(),
+        age: 21
+    })
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello, greet])
+    rocket::build().mount("/", routes![hello, greet, get_person])
 }
